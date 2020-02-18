8505b6e9bf5974dd93c05ff2baa283b6287e7cb0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-18 16:31:48 -0700

    Initial Commit

---
+++ beet_farmer.rb
@@ -0,0 +1,7 @@
+require 'sinatra/base'
+
+class BeetFarmer < Sinatra::Base
+  get '/' do
+    "BEET FARMER READY TO TURNIP THE BEET"
+  end
+end
---
+++ config.ru
@@ -0,0 +1,3 @@
+require './beet_farmer.rb'
+
+run BeetFarmer.new

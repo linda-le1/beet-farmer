02c17e1f2c3d93dd6c7ab801f62363495f4efdff
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-24 23:55:54 -0700

    Refactor controller

--- app/api/v1/beet_farmer.rb
+++ app/api/v1/beet_farmer.rb
@@ -1,32 +1,19 @@
 require 'sinatra/base'
 require 'bundler'
-# require 'pry'
 require 'faraday'
 require 'json'
-# require 'dotenv/load'
 
 require './app/services/spotify_service'
 require './app/poros/playlist'
-require './app/serializers/playlist_serializer'
+require './app/parsers/query_parser'
+require './app/serializers/recommendation_serializer'
 
 Bundler.require
 
 class BeetFarmer < Sinatra::Base
   get('/api/v1/recommend') do
     content_type :json
-
-    mood = params["mood"]
-    cuisine = params["cuisine"]
-    token = params["token"]
-
-    service = SpotifyService.new(token)
-
-    collection = {
-      combos: service.combo_query(mood, cuisine),
-      moods: service.single_query(mood),
-      cuisines: service.single_query(cuisine)
-    }
-
-    PlaylistSerializer.jsonify(mood, cuisine, collection)
+    recommendation = QueryParser.new(params)
+    RecommendationSerializer.jsonify(recommendation)
   end
 end
